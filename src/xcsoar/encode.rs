db34e43f39bd530d57f68c3bae9da8d3b923039d
Tobias Bieniek <tobias@bieniek.cloud>
2021-08-04 16:55:11 +0200

    xcsoar::encode: Implement `Writer` struct

--- src/xcsoar/encode.rs
+++ src/xcsoar/encode.rs
@@ -1,10 +1,13 @@
 use super::fields::*;
 use crate::{File, Record};
 use encoding_rs::mem::{encode_latin1_lossy, is_str_latin1};
+use std::io::{Cursor, Write};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum EncodeError {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
     // the value could not be converted to valid latin1
     #[error("invalid encoding: {0}")]
     InvalidEncoding(String),
@@ -37,76 +40,89 @@ pub enum EncodeError {
 /// "#);
 /// ```
 pub fn encode_file(file: &File) -> Result<Vec<u8>, EncodeError> {
-    let version = format!("{:06x?}", file.version);
-    let records = file
-        .records
-        .iter()
-        .map(|record| encode_record(&record))
-        .collect::<Result<Vec<_>, _>>()?
-        .join("\n");
+    let mut writer = Writer::new(Cursor::new(Vec::new()));
+    writer.write(file)?;
 
-    let content = format!("{}\n{}\n", version, records);
-    Ok(content.into_bytes())
+    let buffer = writer.into_inner().into_inner();
+
+    Ok(buffer)
 }
 
-/// Encodes a single FlarmNet file record.
-///
-/// # Examples
-///
-/// ```
-/// # use flarmnet::Record;
-/// let record = Record {
-///     flarm_id: "3EE3C7".to_string(),
-///     pilot_name: "Tobias Bieniek".to_string(),
-///     airfield: "EDKA".to_string(),
-///     plane_type: "LS6a".to_string(),
-///     registration: "D-0816".to_string(),
-///     call_sign: "SG".to_string(),
-///     frequency: "130.530".to_string(),
-/// };
-///
-/// assert_eq!(
-///     flarmnet::xcsoar::encode_record(&record).unwrap(),
-///     "334545334337546f62696173204269656e69656b2020202020202045444b4120202020202020202020202020202020204c5336612020202020202020202020202020202020442d30383136205347203133302e353330",
-/// );
-/// ```
-pub fn encode_record(record: &Record) -> Result<String, EncodeError> {
-    let fields = vec![
-        encode_str(&record.flarm_id, FLARM_ID_LENGTH)?,
-        encode_str(&record.pilot_name, PILOT_NAME_LENGTH)?,
-        encode_str(&record.airfield, AIRFIELD_LENGTH)?,
-        encode_str(&record.plane_type, PLANE_TYPE_LENGTH)?,
-        encode_str(&record.registration, REGISTRATION_LENGTH)?,
-        encode_str(&record.call_sign, CALL_SIGN_LENGTH)?,
-        encode_str(&record.frequency, FREQUENCY_LENGTH)?,
-    ];
-    Ok(fields.join(""))
+#[derive(Clone)]
+pub struct Writer<W: Write> {
+    writer: W,
 }
 
-fn encode_str(value: &str, length: usize) -> Result<String, EncodeError> {
-    if !is_str_latin1(value) {
-        return Err(EncodeError::InvalidEncoding(value.to_string()));
+impl<W: Write> Writer<W> {
+    pub fn new(inner: W) -> Self {
+        Self { writer: inner }
     }
 
-    let latin1_encoded = encode_latin1_lossy(&value);
-    let mut encoded: Vec<_> = latin1_encoded
-        .iter()
-        .take(length)
-        .map(|byte| format!("{:02x?}", byte))
-        .collect();
+    pub fn write(&mut self, file: &File) -> Result<(), EncodeError> {
+        self.write_version(file.version)?;
+        for record in &file.records {
+            self.write_record(record)?;
+        }
 
-    while encoded.len() < length {
-        encoded.push("20".to_string());
+        Ok(())
     }
 
-    let joined = encoded.join("");
-    Ok(joined)
+    fn write_version(&mut self, version: u32) -> Result<(), EncodeError> {
+        self.writer.write_fmt(format_args!("{:06x?}", version))?;
+        self.writer.write_all(b"\n")?;
+
+        Ok(())
+    }
+
+    fn write_record(&mut self, record: &Record) -> Result<(), EncodeError> {
+        self.write_str(&record.flarm_id, FLARM_ID_LENGTH)?;
+        self.write_str(&record.pilot_name, PILOT_NAME_LENGTH)?;
+        self.write_str(&record.airfield, AIRFIELD_LENGTH)?;
+        self.write_str(&record.plane_type, PLANE_TYPE_LENGTH)?;
+        self.write_str(&record.registration, REGISTRATION_LENGTH)?;
+        self.write_str(&record.call_sign, CALL_SIGN_LENGTH)?;
+        self.write_str(&record.frequency, FREQUENCY_LENGTH)?;
+        self.writer.write_all(b"\n")?;
+
+        Ok(())
+    }
+
+    pub(crate) fn write_str(&mut self, value: &str, length: usize) -> Result<(), EncodeError> {
+        if !is_str_latin1(value) {
+            return Err(EncodeError::InvalidEncoding(value.to_string()));
+        }
+
+        let bytes = encode_latin1_lossy(&value);
+        for byte in bytes.iter().take(length) {
+            self.writer.write_fmt(format_args!("{:02x?}", byte))?;
+        }
+
+        let bytes_len = bytes.len();
+        if bytes_len < length {
+            let placeholders = b"20".repeat(length - bytes_len);
+            self.writer.write_all(&placeholders)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn into_inner(self) -> W {
+        self.writer
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::encode_str;
+    use super::{EncodeError, Writer};
     use insta::assert_debug_snapshot;
+    use std::io::Cursor;
+
+    fn encode_str(value: &str, length: usize) -> Result<String, EncodeError> {
+        let mut writer = Writer::new(Cursor::new(Vec::new()));
+        writer.write_str(value, length)?;
+        let bytes = writer.into_inner().into_inner();
+        Ok(String::from_utf8(bytes).unwrap())
+    }
 
     #[test]
     fn encoding_works() {
