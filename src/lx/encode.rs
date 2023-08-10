0888eda905c8a777bf6face1c51b0e2021b6773d
Tobias Bieniek <tobias@bieniek.cloud>
2023-08-10 14:56:42 +0200

    LX: Only write fields that have values

--- src/lx/encode.rs
+++ src/lx/encode.rs
@@ -76,29 +76,41 @@ impl<W: Write> Writer<W> {
                     vec![("FlarmID".as_bytes(), record.flarm_id.as_bytes())],
                 )))?;
 
-            writer.write_event(Event::Start(BytesStart::new("NAME")))?;
-            writer.write_event(Event::Text(BytesText::new(&record.pilot_name)))?;
-            writer.write_event(Event::End(BytesEnd::new("NAME")))?;
+            if !record.pilot_name.is_empty() {
+                writer.write_event(Event::Start(BytesStart::new("NAME")))?;
+                writer.write_event(Event::Text(BytesText::new(&record.pilot_name)))?;
+                writer.write_event(Event::End(BytesEnd::new("NAME")))?;
+            }
 
-            writer.write_event(Event::Start(BytesStart::new("AIRFIELD")))?;
-            writer.write_event(Event::Text(BytesText::new(&record.airfield)))?;
-            writer.write_event(Event::End(BytesEnd::new("AIRFIELD")))?;
+            if !record.airfield.is_empty() {
+                writer.write_event(Event::Start(BytesStart::new("AIRFIELD")))?;
+                writer.write_event(Event::Text(BytesText::new(&record.airfield)))?;
+                writer.write_event(Event::End(BytesEnd::new("AIRFIELD")))?;
+            }
 
-            writer.write_event(Event::Start(BytesStart::new("TYPE")))?;
-            writer.write_event(Event::Text(BytesText::new(&record.plane_type)))?;
-            writer.write_event(Event::End(BytesEnd::new("TYPE")))?;
+            if !record.plane_type.is_empty() {
+                writer.write_event(Event::Start(BytesStart::new("TYPE")))?;
+                writer.write_event(Event::Text(BytesText::new(&record.plane_type)))?;
+                writer.write_event(Event::End(BytesEnd::new("TYPE")))?;
+            }
 
-            writer.write_event(Event::Start(BytesStart::new("REG")))?;
-            writer.write_event(Event::Text(BytesText::new(&record.registration)))?;
-            writer.write_event(Event::End(BytesEnd::new("REG")))?;
+            if !record.registration.is_empty() {
+                writer.write_event(Event::Start(BytesStart::new("REG")))?;
+                writer.write_event(Event::Text(BytesText::new(&record.registration)))?;
+                writer.write_event(Event::End(BytesEnd::new("REG")))?;
+            }
 
-            writer.write_event(Event::Start(BytesStart::new("COMPID")))?;
-            writer.write_event(Event::Text(BytesText::new(&record.call_sign)))?;
-            writer.write_event(Event::End(BytesEnd::new("COMPID")))?;
+            if !record.call_sign.is_empty() {
+                writer.write_event(Event::Start(BytesStart::new("COMPID")))?;
+                writer.write_event(Event::Text(BytesText::new(&record.call_sign)))?;
+                writer.write_event(Event::End(BytesEnd::new("COMPID")))?;
+            }
 
-            writer.write_event(Event::Start(BytesStart::new("FREQUENCY")))?;
-            writer.write_event(Event::Text(BytesText::new(&record.frequency)))?;
-            writer.write_event(Event::End(BytesEnd::new("FREQUENCY")))?;
+            if !record.frequency.is_empty() {
+                writer.write_event(Event::Start(BytesStart::new("FREQUENCY")))?;
+                writer.write_event(Event::Text(BytesText::new(&record.frequency)))?;
+                writer.write_event(Event::End(BytesEnd::new("FREQUENCY")))?;
+            }
 
             writer.write_event(Event::End(BytesEnd::new("FLARMDATA")))?;
             writer.write_event(Event::Text(BytesText::from_escaped("\n")))?;
