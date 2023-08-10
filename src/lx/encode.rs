210bcbed3d180d64452d9b02860ffe32d88f2755
Tobias Bieniek <tobias@bieniek.cloud>
2023-08-10 12:33:51 +0200

    LX: Use up-to-date `quick-xml` dependency for encoding

--- src/lx/encode.rs
+++ src/lx/encode.rs
@@ -1,7 +1,7 @@
 use crate::lx::cipher;
 use crate::File;
-use minidom::quick_xml;
-use minidom::quick_xml::events::{BytesDecl, BytesEnd, BytesStart, BytesText, Event};
+use quick_xml;
+use quick_xml::events::{BytesDecl, BytesEnd, BytesStart, BytesText, Event};
 use std::io::{Cursor, Write};
 use thiserror::Error;
 
@@ -60,63 +60,63 @@ impl<W: Write> Writer<W> {
     pub fn write(&mut self, file: &File) -> Result<(), EncodeError> {
         let writer = &mut self.xml_writer;
 
-        writer.write_event(Event::Decl(BytesDecl::new(b"1.0", Some(b"UTF-8"), None)))?;
-        writer.write_event(Event::Text(BytesText::from_escaped(b"\n" as &[u8])))?;
+        writer.write_event(Event::Decl(BytesDecl::new("1.0", Some("UTF-8"), None)))?;
+        writer.write_event(Event::Text(BytesText::from_escaped("\n")))?;
 
         let version = format!("{:06x?}", file.version);
         writer.write_event(Event::Start(
-            BytesStart::borrowed_name(b"FLARMNET")
+            BytesStart::new("FLARMNET")
                 .with_attributes(vec![("Version".as_bytes(), version.as_bytes())]),
         ))?;
-        writer.write_event(Event::Text(BytesText::from_escaped(b"\n" as &[u8])))?;
+        writer.write_event(Event::Text(BytesText::from_escaped("\n")))?;
 
         for record in &file.records {
-            writer.write_event(Event::Start(
-                BytesStart::borrowed_name(b"FLARMDATA")
-                    .with_attributes(vec![("FlarmID".as_bytes(), record.flarm_id.as_bytes())]),
-            ))?;
-            writer.write_event(Event::Text(BytesText::from_escaped(b"\n" as &[u8])))?;
+            writer
+                .write_event(Event::Start(BytesStart::new("FLARMDATA").with_attributes(
+                    vec![("FlarmID".as_bytes(), record.flarm_id.as_bytes())],
+                )))?;
+            writer.write_event(Event::Text(BytesText::from_escaped("\n")))?;
 
-            writer.write_event(Event::Text(BytesText::from_escaped(b"\t" as &[u8])))?;
-            writer.write_event(Event::Start(BytesStart::borrowed_name(b"NAME")))?;
-            writer.write_event(Event::Text(BytesText::from_plain_str(&record.pilot_name)))?;
-            writer.write_event(Event::End(BytesEnd::borrowed(b"NAME")))?;
-            writer.write_event(Event::Text(BytesText::from_escaped(b"\n" as &[u8])))?;
+            writer.write_event(Event::Text(BytesText::from_escaped("\t")))?;
+            writer.write_event(Event::Start(BytesStart::new("NAME")))?;
+            writer.write_event(Event::Text(BytesText::new(&record.pilot_name)))?;
+            writer.write_event(Event::End(BytesEnd::new("NAME")))?;
+            writer.write_event(Event::Text(BytesText::from_escaped("\n")))?;
 
-            writer.write_event(Event::Text(BytesText::from_escaped(b"\t" as &[u8])))?;
-            writer.write_event(Event::Start(BytesStart::borrowed_name(b"AIRFIELD")))?;
-            writer.write_event(Event::Text(BytesText::from_plain_str(&record.airfield)))?;
-            writer.write_event(Event::End(BytesEnd::borrowed(b"AIRFIELD")))?;
-            writer.write_event(Event::Text(BytesText::from_escaped(b"\n" as &[u8])))?;
+            writer.write_event(Event::Text(BytesText::from_escaped("\t")))?;
+            writer.write_event(Event::Start(BytesStart::new("AIRFIELD")))?;
+            writer.write_event(Event::Text(BytesText::new(&record.airfield)))?;
+            writer.write_event(Event::End(BytesEnd::new("AIRFIELD")))?;
+            writer.write_event(Event::Text(BytesText::from_escaped("\n")))?;
 
-            writer.write_event(Event::Text(BytesText::from_escaped(b"\t" as &[u8])))?;
-            writer.write_event(Event::Start(BytesStart::borrowed_name(b"TYPE")))?;
-            writer.write_event(Event::Text(BytesText::from_plain_str(&record.plane_type)))?;
-            writer.write_event(Event::End(BytesEnd::borrowed(b"TYPE")))?;
-            writer.write_event(Event::Text(BytesText::from_escaped(b"\n" as &[u8])))?;
+            writer.write_event(Event::Text(BytesText::from_escaped("\t")))?;
+            writer.write_event(Event::Start(BytesStart::new("TYPE")))?;
+            writer.write_event(Event::Text(BytesText::new(&record.plane_type)))?;
+            writer.write_event(Event::End(BytesEnd::new("TYPE")))?;
+            writer.write_event(Event::Text(BytesText::from_escaped("\n")))?;
 
-            writer.write_event(Event::Text(BytesText::from_escaped(b"\t" as &[u8])))?;
-            writer.write_event(Event::Start(BytesStart::borrowed_name(b"REG")))?;
-            writer.write_event(Event::Text(BytesText::from_plain_str(&record.registration)))?;
-            writer.write_event(Event::End(BytesEnd::borrowed(b"REG")))?;
-            writer.write_event(Event::Text(BytesText::from_escaped(b"\n" as &[u8])))?;
+            writer.write_event(Event::Text(BytesText::from_escaped("\t")))?;
+            writer.write_event(Event::Start(BytesStart::new("REG")))?;
+            writer.write_event(Event::Text(BytesText::new(&record.registration)))?;
+            writer.write_event(Event::End(BytesEnd::new("REG")))?;
+            writer.write_event(Event::Text(BytesText::from_escaped("\n")))?;
 
-            writer.write_event(Event::Text(BytesText::from_escaped(b"\t" as &[u8])))?;
-            writer.write_event(Event::Start(BytesStart::borrowed_name(b"COMPID")))?;
-            writer.write_event(Event::Text(BytesText::from_plain_str(&record.call_sign)))?;
-            writer.write_event(Event::End(BytesEnd::borrowed(b"COMPID")))?;
-            writer.write_event(Event::Text(BytesText::from_escaped(b"\n" as &[u8])))?;
+            writer.write_event(Event::Text(BytesText::from_escaped("\t")))?;
+            writer.write_event(Event::Start(BytesStart::new("COMPID")))?;
+            writer.write_event(Event::Text(BytesText::new(&record.call_sign)))?;
+            writer.write_event(Event::End(BytesEnd::new("COMPID")))?;
+            writer.write_event(Event::Text(BytesText::from_escaped("\n")))?;
 
-            writer.write_event(Event::Text(BytesText::from_escaped(b"\t" as &[u8])))?;
-            writer.write_event(Event::Start(BytesStart::borrowed_name(b"FREQUENCY")))?;
-            writer.write_event(Event::Text(BytesText::from_plain_str(&record.frequency)))?;
-            writer.write_event(Event::End(BytesEnd::borrowed(b"FREQUENCY")))?;
-            writer.write_event(Event::Text(BytesText::from_escaped(b"\n" as &[u8])))?;
+            writer.write_event(Event::Text(BytesText::from_escaped("\t")))?;
+            writer.write_event(Event::Start(BytesStart::new("FREQUENCY")))?;
+            writer.write_event(Event::Text(BytesText::new(&record.frequency)))?;
+            writer.write_event(Event::End(BytesEnd::new("FREQUENCY")))?;
+            writer.write_event(Event::Text(BytesText::from_escaped("\n")))?;
 
-            writer.write_event(Event::End(BytesEnd::borrowed(b"FLARMDATA")))?;
-            writer.write_event(Event::Text(BytesText::from_escaped(b"\n" as &[u8])))?;
+            writer.write_event(Event::End(BytesEnd::new("FLARMDATA")))?;
+            writer.write_event(Event::Text(BytesText::from_escaped("\n")))?;
         }
-        writer.write_event(Event::End(BytesEnd::borrowed(b"FLARMNET")))?;
+        writer.write_event(Event::End(BytesEnd::new("FLARMNET")))?;
 
         Ok(())
     }
